442016029b38f51b766d263746a1e40b4c689477
stunstun <wjdsupj@gmail.com>
2017-03-17 02:00:00 +0700

    update hash table practice

--- tests/test_data_structures.py
+++ tests/test_data_structures.py
@@ -15,3 +15,17 @@ class TestStack(unittest.TestCase):
         self.assertEqual(self.stack.pop(), 6)
         self.assertEqual(self.stack.is_empty(), False)
         self.assertEqual(self.stack.size(), 2)
+
+
+class TestHashTable(unittest.TestCase):
+
+    def test_string(self):
+        name = 'minhyeok'
+        hash1 = name.__hash__()
+        name = 'minhyeok' + ' jung'
+        hash2 = name.__hash__()
+        full_name = 'minhyeok jung'
+        hash3 = full_name.__hash__()
+
+        self.assertNotEqual(hash1, hash2)
+        self.assertEqual(hash2, hash3)
